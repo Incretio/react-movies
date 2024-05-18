7808d8e31730ab96401c736f1c574b23170f28f7
incretio <incretio@gmail.com>
2024-05-18 09:54:39 +0300

    Updates

---
+++ precache-manifest.57cbbe2bcad77adb2439bf37681d360d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff7fd1753a79e143712828b919d3257",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "d33c56729b7bed6c33a5",
+    "url": "/react-movies/static/css/main.d7b80ca7.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/react-movies/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "d33c56729b7bed6c33a5",
+    "url": "/react-movies/static/js/main.e5098582.chunk.js"
+  },
+  {
+    "revision": "cdb8474b72bd7dff1c63",
+    "url": "/react-movies/static/js/runtime~main.bf100f6d.js"
+  }
+]);
